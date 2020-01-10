36e747f08f805a1917de7a82cb714e7e4490df6a
川嶋康太 <kawashimakouta@kawashimakoutas-MacBook-Pro.local>
2020-01-10 23:58:33 +0900

    F#サンプルアプリ

---
+++ testApp01/iOS/AppDelegate.fs
@@ -0,0 +1,22 @@
+﻿namespace testApp01.iOS
+
+open System
+open UIKit
+open Foundation
+open Xamarin.Forms
+open Xamarin.Forms.Platform.iOS
+
+[<Register ("AppDelegate")>]
+type AppDelegate () =
+    inherit FormsApplicationDelegate ()
+
+    override this.FinishedLaunching (app, options) =
+        Forms.Init()
+        this.LoadApplication (new testApp01.App())
+        base.FinishedLaunching(app, options)
+
+module Main =
+    [<EntryPoint>]
+    let main args =
+        UIApplication.Main(args, null, "AppDelegate")
+        0
---
+++ testApp01/testApp01/MainPage.xaml.fs
@@ -0,0 +1,44 @@
+﻿namespace testApp01
+
+open Xamarin.Forms
+open Xamarin.Forms.Xaml
+open System
+
+type MainPage() =
+    inherit ContentPage()    
+    let _ = base.LoadFromXaml(typeof<MainPage>)
+
+    let entry = Entry(Placeholder = "数を入力してください")
+    let button = Button(Text ="平方根を計算する")
+    
+    let updateEntry s =
+        entry.Text <- s
+
+    let tryFloat x =
+        match System.Double.TryParse x with
+        | true, x -> Ok x
+        | false, _ -> Error (sprintf "%sを変換することができませんでした" x)
+
+    let toString (f: float) =
+        string f
+
+    let toSqrt x =
+        match x with
+        | x when x < 0. -> Error (sprintf "%fは負の数なので計算できません" x)
+        | _ -> Ok (Math.Sqrt x)
+
+    let calculate _ =
+        entry.Text 
+        |> tryFloat 
+        |> Result.bind toSqrt
+        |> Result.map toString
+        |> function
+            | Ok x -> updateEntry x
+            | Error x -> updateEntry x
+
+    do 
+        button.Clicked.Add calculate
+        let stack = StackLayout(VerticalOptions = LayoutOptions.CenterAndExpand)
+        stack.Children.Add entry
+        stack.Children.Add button
+        base.Content <- stack
